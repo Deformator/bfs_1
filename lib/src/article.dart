996a5c8247a2c0024d97a4698cac9e55306cc30b
Andrii Damm <andrii@unionms-iMac.local>
2019-02-22 17:01:13 -0500

    finish

---
+++ lib/src/article.dart
@@ -0,0 +1,130 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article(
+      {this.text,
+      this.domain,
+      this.by,
+      this.age,
+      this.score,
+      this.commentsCount});
+}
+
+final articles = [
+  new Article(
+      text:
+          "Circular Shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-5",
+      domain: "wiley.com",
+      by: "zdw",
+      age: "3 hours",
+      score: 177,
+      commentsCount: 62),
+  new Article(
+      text: "BMW says electric car mass production not viable until 2020",
+      domain: "reuters.com",
+      by: "Mononokay",
+      age: "2 hours",
+      score: 81,
+      commentsCount: 128),
+  new Article(
+      text: "TCP Tracepoints have arrived in Linux",
+      domain: "brendangregg.com",
+      by: "brendangregg",
+      age: "1 hours",
+      score: 35,
+      commentsCount: 0),
+  new Article(
+      text:
+          "Section 230: A Key Legal Shield for Facebook, Google Is About to Change",
+      domain: "npr.org",
+      by: "zdw",
+      age: "5 hours",
+      score: 156,
+      commentsCount: 66),
+  new Article(
+      text: "A Visiting Star Jostled Our Solar System 70,700 Years Ago",
+      domain: "gizmodo.com",
+      by: "rbanffy",
+      age: "7 hours",
+      score: 175,
+      commentsCount: 60),
+        new Article(
+      text:
+          "Circular Shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-5",
+      domain: "wiley.com",
+      by: "zdw",
+      age: "3 hours",
+      score: 177,
+      commentsCount: 62),
+  new Article(
+      text: "BMW says electric car mass production not viable until 2020",
+      domain: "reuters.com",
+      by: "Mononokay",
+      age: "2 hours",
+      score: 81,
+      commentsCount: 128),
+  new Article(
+      text: "TCP Tracepoints have arrived in Linux",
+      domain: "brendangregg.com",
+      by: "brendangregg",
+      age: "1 hours",
+      score: 35,
+      commentsCount: 0),
+  new Article(
+      text:
+          "Section 230: A Key Legal Shield for Facebook, Google Is About to Change",
+      domain: "npr.org",
+      by: "zdw",
+      age: "5 hours",
+      score: 156,
+      commentsCount: 66),
+  new Article(
+      text: "A Visiting Star Jostled Our Solar System 70,700 Years Ago",
+      domain: "gizmodo.com",
+      by: "rbanffy",
+      age: "7 hours",
+      score: 175,
+      commentsCount: 60),
+        new Article(
+      text:
+          "Circular Shock Acoustic Waves in Ionosphere Triggered by Launch of Formosat-5",
+      domain: "wiley.com",
+      by: "zdw",
+      age: "3 hours",
+      score: 177,
+      commentsCount: 62),
+  new Article(
+      text: "BMW says electric car mass production not viable until 2020",
+      domain: "reuters.com",
+      by: "Mononokay",
+      age: "2 hours",
+      score: 81,
+      commentsCount: 128),
+  new Article(
+      text: "TCP Tracepoints have arrived in Linux",
+      domain: "brendangregg.com",
+      by: "brendangregg",
+      age: "1 hours",
+      score: 35,
+      commentsCount: 0),
+  new Article(
+      text:
+          "Section 230: A Key Legal Shield for Facebook, Google Is About to Change",
+      domain: "npr.org",
+      by: "zdw",
+      age: "5 hours",
+      score: 156,
+      commentsCount: 66),
+  new Article(
+      text: "A Visiting Star Jostled Our Solar System 70,700 Years Ago",
+      domain: "gizmodo.com",
+      by: "rbanffy",
+      age: "7 hours",
+      score: 175,
+      commentsCount: 60),
+];
